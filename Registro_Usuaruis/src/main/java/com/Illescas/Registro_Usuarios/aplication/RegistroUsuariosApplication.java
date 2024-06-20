59b5576698ed0db72ad7ead55c49df16b050e60c
MichaelIllescas <joni.illes@hotmail.com>
2024-06-20 01:16:40 -0300

    Commit inicial con eclipse

---
+++ Registro_Usuaruis/src/main/java/com/Illescas/Registro_Usuarios/aplication/RegistroUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.Illescas.Registro_Usuarios.aplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroUsuariosApplication.class, args);
+	}
+
+}
---
+++ Registro_Usuaruis/src/main/java/com/Illescas/Registro_Usuarios/aplication/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.Illescas.Registro_Usuarios.aplication;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(RegistroUsuariosApplication.class);
+	}
+
+}
